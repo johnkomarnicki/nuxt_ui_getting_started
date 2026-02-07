ea4b50befbb83dfc8d04c633b99800828eea3490
John <komarnickij@yahoo.com>
2026-02-07 12:19:59 -0500

    template

---
+++ app/app.config.ts
@@ -0,0 +1,14 @@
+export default defineAppConfig({
+  ui: {
+    button: {
+      variants: {
+        size: {
+          // 'px-2.5 py-1.5 text-sm gap-1.5'
+          //  Merges to this
+          // 'px-3 py-3 text-sm gap-1.5'
+          md: "px-3 py-3",
+        },
+      },
+    },
+  },
+});
